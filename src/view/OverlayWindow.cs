89a16890a5a87dc5aa49102a863348353558057a
ewasion137 <formanban94@gmail.com>
2026-06-16 22:08:05 +0300

    Overlay Window cleared and more easier, better comments

--- src/view/OverlayWindow.cs
+++ src/view/OverlayWindow.cs
@@ -1,69 +1,37 @@
 using System;
-using System.Runtime.InteropServices;
 using System.Windows;
-using System.Windows.Controls;
 using System.Windows.Interop;
-using System.Windows.Media;
+using System.Runtime.InteropServices;
 
 namespace TypeAestetic.View;
 
 public class OverlayWindow : Window
 {
-    private const int GWL_EXSTYLE = -20;
-    private const int WS_EX_TRANSPARENT = 0x00000020;
-    private const int WS_EX_LAYERED = 0x00080000;
-
-    private readonly Canvas _canvas;
-
     public OverlayWindow()
     {
-        // Config
         WindowStyle = WindowStyle.None;
         AllowsTransparency = true;
-        Background = Brushes.Transparent;
+        Background = System.Windows.Media.Brushes.Transparent;
         Topmost = true;
         ShowInTaskbar = false;
-        
-        // Fullscreen
-        Left = 0;
-        Top = 0;
-        Width = SystemParameters.PrimaryScreenWidth;
-        Height = SystemParameters.PrimaryScreenHeight;
+        SizeToContent = SizeToContent.WidthAndHeight;
 
-        _canvas = new Canvas();
-        Content = _canvas;
+        // Position at bottom-right
+        Left = SystemParameters.PrimaryScreenWidth - 450; 
+        Top = SystemParameters.PrimaryScreenHeight - 250;
     }
 
     protected override void OnSourceInitialized(EventArgs e)
     {
         base.OnSourceInitialized(e);
-        
-        // WS_EX_TRANSPARENT
-        IntPtr hwnd = new WindowInteropHelper(this).Handle;
-        int extendedStyle = GetWindowLong(hwnd, GWL_EXSTYLE);
-        SetWindowLong(hwnd, GWL_EXSTYLE, extendedStyle | WS_EX_TRANSPARENT | WS_EX_LAYERED);
+        // Make it click-through using Win32
+        var hwnd = new WindowInteropHelper(this).Handle;
+        uint exStyle = GetWindowLong(hwnd, -20);
+        SetWindowLong(hwnd, -20, exStyle | 0x00000020 | 0x00080000);
     }
-
-    public void SpawnLetter(char character)
-    {
-        // Test
-        var random = new Random();
-        var textBlock = new TextBlock
-        {
-            Text = character.ToString(),
-            FontSize = 48,
-            Foreground = Brushes.White,
-            FontWeight = FontWeights.Bold
-        };
-
-        Canvas.SetLeft(textBlock, random.Next(100, (int)Width - 100));
-        Canvas.SetTop(textBlock, random.Next(100, (int)Height - 100));
-        _canvas.Children.Add(textBlock);
-    }
-
-    [DllImport("user32.dll", SetLastError = true)]
-    private static extern int GetWindowLong(IntPtr hWnd, int nIndex);
 
     [DllImport("user32.dll")]
-    private static extern int SetWindowLong(IntPtr hWnd, int nIndex, int dwNewLong);
+    static extern uint GetWindowLong(IntPtr hWnd, int nIndex);
+    [DllImport("user32.dll")]
+    static extern int SetWindowLong(IntPtr hWnd, int nIndex, uint dwNewLong);
 }
