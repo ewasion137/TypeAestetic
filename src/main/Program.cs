add399c9305712cc7fba2176c3388c75ee01d049
ewasion137 <formanban94@gmail.com>
2026-06-16 21:53:56 +0300

    Replaced makefile and changed Program.cs (and changed namespace location again)

--- src/main/Program.cs
+++ src/main/Program.cs
@@ -1,17 +1,25 @@
 using System;
 using System.Windows;
+using TypeAestetic.Hooks;
 using TypeAestetic.View;
 
-namespace TypeAestetic.Main
+namespace TypeAestetic.Main;
+
+public static class Program
 {
-    public class Program
+    [STAThread]
+    public static void Main()
     {
-        [STAThread]
-        public static void Main()
+        var app = new Application();
+        var window = new OverlayWindow();
+        using var hook = new KeyboardHook();
+
+        hook.KeyPressed += (character) =>
         {
-            var app = new Application();
-            var window = new OverlayWindow();
-            app.Run(window);
-        }
+            window.Dispatcher.Invoke(() => window.SpawnLetter(character));
+        };
+
+        hook.Install();
+        app.Run(window);
     }
 }
